d97c61fe683f8bcfcea08d3e3be191f9e0d934f8
Beya Mulumba <beyamulumbajm@gmail.com>
2025-12-04 14:51:19 +0100

    Initial commit of Chatbot project

---
+++ src/bot/SimpleBot.java
@@ -0,0 +1,12 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+        String botName = "Aid";
+        int birthYear = 2023;
+
+        System.out.printf("Hello! My name is %s.\n", botName);
+        System.out.printf("I was created in %d.", birthYear);
+    }
+}
